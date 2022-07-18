b77008d56ad9dbc8748c6a2af2f48104508a0477
BahaAbuAlkibash <baha.ra3d@gmail.com>
2022-07-18 09:10:18 +0300

    Add project files.

---
+++ ECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private StoreContext storeContext;
+
+        public ProductsController(StoreContext context)
+        {
+            this.storeContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await storeContext.Products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return  Ok( await storeContext.Products.FindAsync(id));
+        }
+    }
+}
---
+++ ECommerce/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+
+        
+        public DbSet<Product> Products{ get; set; }
+    }
+}
